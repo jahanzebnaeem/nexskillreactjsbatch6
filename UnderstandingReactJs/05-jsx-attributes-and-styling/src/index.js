d8b5ff8f604189226c74d2b7d6e01d6bd37e5edc
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-09-10 16:25:57 +0500

    Lecture 7
    Styling in react.

--- UnderstandingReactJs/05-jsx-attributes-and-styling/src/index.js
+++ UnderstandingReactJs/05-jsx-attributes-and-styling/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
+    <h1 className="heading" contentEditable="true" spellCheck="false">My Favourite Foods</h1>
+    {/* <ul>
       <li>Bacon</li>
       <li>Jamon</li>
       <li>Noodles</li>
-    </ul>
+    </ul> */}
+
+    <img alt="random" src={img + '?grayscale'} />
   </div>,
   document.getElementById("root")
 );
--- UnderstandingReactJs/06-inline-styling-in-jsx/src/index.js
+++ UnderstandingReactJs/06-inline-styling-in-jsx/src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle = {
+	color: "red",
+	fontSize: 20,
+	border: "1px solid black",
+};
+
+
+ReactDOM.render(
+	// <h1 style={{ color: "red", }}>Hello World!</h1>,
+	<h1 style={customStyle}>Hello World!</h1>,
+	document.getElementById("root")
+);
