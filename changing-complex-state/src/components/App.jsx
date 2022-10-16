75d481209c74458aacf8ade8fe61cd89eb93a3bb
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-10-16 18:21:35 +0500

    Lecture 16
    Working with complex states.

--- changing-complex-state/src/components/App.jsx
+++ changing-complex-state/src/components/App.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setFullName(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          name="fName"
+          placeholder="First Name"
+          onChange={handleChange}
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          onChange={handleChange}
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
