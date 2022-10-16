bde10f494b8e8b76ae7551cf4c338b9ae6143c4d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-10-16 13:56:59 +0500

    Lecture 16
    Working with react forms.

--- react-forms/src/components/App.jsx
+++ react-forms/src/components/App.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+// jshint esversion:6
+
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(event) {
+    // console.log("Changed");
+    // console.log(event.target.value);
+    // console.log(event.target.placeholder);
+    // console.log(event.target.type);
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setHeading(name);
+    // console.log(name);
+
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          type="text"
+          placeholder="What's your name?"
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
