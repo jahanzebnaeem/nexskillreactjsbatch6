534ed433d16cd748f79250337580e966a039a38d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-10-16 13:26:19 +0500

    Lecture 16
    Event understanding.

--- event-handling-in-react/src/components/App.jsx
+++ event-handling-in-react/src/components/App.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+// jshint esversion:6
+import React, { useState } from "react";
 
 function App() {
+  const [headerText, setHeaderText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    // console.log("clicked");
+    setHeaderText("Submitted");
+  }
+
+  function handleMouseOver() {
+    // console.log("Moused Over");
+    setMouseOver(true);
+    // setHeaderText("Enter button");
+  }
+
+  function handleMouseOut() {
+    // console.log("Moused Out");
+    setMouseOver(false);
+    // setHeaderText("Leave button");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headerText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
